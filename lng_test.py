e6825ac8e8a925023d28a7958f1e3e349d2589d0
HCL <lhc.Lu2002@outlook.com>
2023-05-23 21:20:25 +0800

    kmeans1.0

--- lng_test.py
+++ lng_test.py
@@ -19,18 +19,26 @@ data = data_loader.load_csv()
 
 start = time.time()
 print("start")
-clustering_kmeans = KMeans(n_clusters=400, init='random',max_iter=40).fit(data)
+clustering_kmeans = KMeans(n_clusters=800, init='random',verbose=1,max_iter=30).fit(data)
 end = time.time()
-
 # 将模型保存到文件
 with open('kmeans_model.pkl', 'wb') as f:
     pickle.dump(clustering_kmeans, f)
-
 print("Kmeans_time:",end-start)
+
+
+'''
+# 加载保存的模型
+with open('kmeans_model.pkl', 'rb') as f:
+    clustering_kmeans = pickle.load(f)
+'''
+
 print("总数: ", len(clustering_kmeans.labels_))
 print("聚类中心点数: ", len(clustering_kmeans.cluster_centers_))
 for center in clustering_kmeans.cluster_centers_:
      print("中心坐标：",center ,"\n")
+inertia = clustering_kmeans.inertia_
+print("方差",inertia)
 
 
 
