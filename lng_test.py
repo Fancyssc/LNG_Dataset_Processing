75bba9f5d3940efe786e797a54fd494395c7ed1e
HCL <lhc.Lu2002@outlook.com>
2023-05-23 15:57:02 +0800

    kmeans

--- lng_test.py
+++ lng_test.py
@@ -1,25 +1,36 @@
 import time
 import os
 import sys
+import pickle
+
 sys.path.append(".")
 from sklearn.cluster import KMeans
 
 from util_pakage.Pre_process import preprocess, convert_to_labeledcsv
 from util_pakage.IOUtil import CSVDataLoader
 
-if __name__ == '__main__':
-    convert_to_labeledcsv()
-    preprocess()
-    cwd = os.getcwd()
-    path1 = cwd + "/data/filter_lng.csv"
-    data_loader = CSVDataLoader(path1)
-    data = data_loader.load_csv()
-    '''
-    start = time.time()
-    clustering_kmeans = KMeans(n_clusters=400, init='random',max_iter=40).fit(data)
-    end = time.time()
-    print("Kmeans_time:"+end-start)
-    for center in clustering_kmeans.cluster_centers_:
-         print("中心坐标："+center +"\n")
-         '''
+#convert_to_labeledcsv()
+#preprocess()
+cwd = os.getcwd()
+path1 = cwd + "/data/filter_lng.csv"
+data_loader = CSVDataLoader(path1)
+print("start")
+data = data_loader.load_csv()
+
+start = time.time()
+print("start")
+clustering_kmeans = KMeans(n_clusters=400, init='random',max_iter=40).fit(data)
+end = time.time()
+
+# 将模型保存到文件
+with open('kmeans_model.pkl', 'wb') as f:
+    pickle.dump(clustering_kmeans, f)
+
+print("Kmeans_time:",end-start)
+print("总数: ", len(clustering_kmeans.labels_))
+print("聚类中心点数: ", len(clustering_kmeans.cluster_centers_))
+for center in clustering_kmeans.cluster_centers_:
+     print("中心坐标：",center ,"\n")
+
+
 
